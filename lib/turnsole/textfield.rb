76d0a7b10b0d05deb95f6bb55887a231abfc2f15
William Morgan <wmorgan-github@masanjin.net>
2011-04-30 15:32:35 -0700

    fix up textfield character position logic

--- lib/turnsole/textfield.rb
+++ lib/turnsole/textfield.rb
@@ -31,7 +31,15 @@ class TextField
     @w, @y, @x, @width = window, y, x, width
     @question = question
     @answer = default || ""
-    @curpos = @answer.display_width
+
+    ## important! curpos is the position in characters, not in bytes or display
+    ## characters. @answer.length is the rightmost position.
+    @curpos = @answer.length
+
+    ## a horrible thing we use to clean up after intermediate states where we
+    ## have partial sequences of a multibyte character
+    @curpos_excess = 0
+
     @completion_block = block
     @completion_state = nil
     @completions = []
@@ -43,7 +51,13 @@ class TextField
   end
 
   def position_cursor!
-    Ncurses.move @y, @x + @question.display_width + @curpos
+    begin
+      Ncurses.move @y, @x + (@question + @answer[0 ... @curpos]).display_width
+    rescue ArgumentError
+      ## we see this if we have a partial multibyte character in the answer.
+      ## we'll just ignore the issue and not move the cursor, assuming that
+      ## successive bytes are coming soon!
+    end
   end
 
   def deactivate!
@@ -74,7 +88,7 @@ class TextField
             @answer += " "
             @completion_state = :clear
           end
-          @curpos = @answer.display_width # to the end
+          @curpos = @answer.length # to the end
           position_cursor!
         end
         if c.size > 1
@@ -95,23 +109,22 @@ class TextField
 
     case c
     when Ncurses::KEY_LEFT; @curpos = [0, @curpos - 1].max
-    when Ncurses::KEY_RIGHT; @curpos = [@curpos + 1, @width - 1].min
-    when Ncurses::KEY_DC, ?\C-d.ord; @answer = @answer[0 ... @curpos] + @answer[(@curpos + 1) .. -1]
+    when Ncurses::KEY_RIGHT; @curpos = [@curpos + 1, @answer.length].min
+    when Ncurses::KEY_DC, ?\C-d.ord; @answer = @answer[0 ... @curpos] + (@answer[(@curpos + 1) .. -1] || "")
     when Ncurses::KEY_BACKSPACE, 127
       if @curpos > 0
-        diff = @answer[@curpos] ? @answer[@curpos].chr.display_width : 1
-        @answer = @answer[0 ... (@curpos - 1)] + @answer[@curpos .. -1]
-        @curpos -= diff
+        @answer = @answer[0 ... (@curpos - 1)] + (@answer[@curpos .. -1] || "")
+        @curpos -= 1
       end
     when ?\C-a.ord, Ncurses::KEY_HOME; @curpos = 0
-    when ?\C-e.ord, Ncurses::KEY_END; @curpos = [@answer.display_width, @width - 2].min
+    when ?\C-e.ord, Ncurses::KEY_END; @curpos = @answer.length
     when ?\C-k.ord; @answer = @answer[0 ... @curpos]
     when ?\C-u.ord
-      @answer = @answer[@curpos .. -1]
+      @answer = (@answer[@curpos .. -1] || "")
       @curpos = 0
     when ?\C-w.ord
       space_pos = @answer.rindex(/\s/, @curpos) || 0
-      @answer = @answer[0 ... space_pos] + @answer[@curpos .. -1]
+      @answer = @answer[0 ... space_pos] + (@answer[@curpos .. -1] || "")
       @curpos = space_pos
       #Ncurses::Form.form_driver @form, Ncurses::Form::REQ_PREV_CHAR
       #Ncurses::Form.form_driver @form, Ncurses::Form::REQ_DEL_WORD
@@ -126,11 +139,30 @@ class TextField
         #debug "history after #{@history.inspect}"
       end
     else
-      width = c.chr.display_width
-      if width > 0
-        @answer = @answer[0 ... @curpos] + c.chr + @answer[@curpos .. -1]
-        @answer.safely_mark_encoding! @context.encoding
-        @curpos += c.chr.display_width
+      ch = c.chr.safely_mark_encoding! @context.encoding
+      @answer = @answer[0 ... @curpos] + ch + (@answer[@curpos .. -1] || "")
+      @answer.safely_mark_encoding! @context.encoding
+      @curpos += 1
+
+      ## here is a terrible hack, due to the fact that curses gives us
+      ## multibyte characters one character at a time.
+      ##
+      ## if we can't calculate the display_width of the
+      ## answer, then we probably have just added a partial character to the
+      ## string. (we could do this more elegantly in ruby 1.9, but this should
+      ## be 1.8-compatible.)
+      ##
+      ## in this case, we will advance curpos anyways, so that later bytes of
+      ## the same character end up in the right place, but we will record how
+      ## much excess we have. if the string has a display_width, then we assume
+      ## we have the full character, and we remove any excess.
+
+      begin
+        @answer.display_width
+        @curpos -= @curpos_excess
+        @curpos_excess = 0
+      rescue ArgumentError
+        @curpos_excess += 1
       end
     end
 
